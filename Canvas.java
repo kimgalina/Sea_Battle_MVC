a76a3cdae26a9f372735db8ab5304f7173193256
Diyor <diyor@Diyors-MacBook-Air.local>
2023-11-28 15:02:59 +0600

    Added field validation method

--- Canvas.java
+++ Canvas.java
@@ -1,6 +1,6 @@
 import javax.swing.JPanel;
 
-public class Canvas extends JPanel{
+public class Canvas extends JPanel {
     private Model model;
 
     public Canvas(Model model) {
--- Viewer.java
+++ Viewer.java
@@ -1,6 +1,7 @@
 public class Viewer {
     private Controller controller;
     private Canvas canvas;
+
     public Viewer() {
         controller = new Controller(this);
         canvas = new Canvas(controller.getModel());
