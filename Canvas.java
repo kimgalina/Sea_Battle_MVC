5b64612111b77e7522bb00d1f619539d74d778a1
Азирет Раманкулов <Aziret5265@gmail.com>
2023-11-29 15:07:40 +0600

    added numbers and letters to the fields

--- Canvas.java
+++ Canvas.java
@@ -23,38 +23,73 @@ public class Canvas extends JPanel {
     }
 
     private void drawGrid(Graphics2D g2d) {
-       int cellSize = 40;
-       int boardSize = model.getBoardSize();
+        int cellSize = 40;
+        int boardSize = model.getBoardSize();
 
-       // Вычисляем смещение по горизонтали и вертикали для центрирования левой половины поля
-       int xOffset = (getWidth() - boardSize * cellSize * 2) / 4; // Общее смещение
-       int yOffset = (getHeight() - boardSize * cellSize) / 2; // Вертикальное смещение
+        // Вычисляем смещение по горизонтали и вертикали для центрирования левой половины поля
+        int xOffset = (getWidth() - boardSize * cellSize * 2) / 4; // Общее смещение
+        int yOffset = (getHeight() - boardSize * cellSize) / 2; // Вертикальное смещение
 
-       // Рисуем сетку для вашего поля
-       for (int i = 0; i <= boardSize; i++) {
-           g2d.drawLine(i * cellSize + xOffset, yOffset, i * cellSize + xOffset, boardSize * cellSize + yOffset);
-       }
+        // Рисуем сетку для вашего поля
+        for (int i = 0; i <= boardSize; i++) {
+            g2d.drawLine(i * cellSize + xOffset, yOffset, i * cellSize + xOffset, boardSize * cellSize + yOffset);
 
-       for (int i = 0; i <= boardSize; i++) {
-           g2d.drawLine(xOffset, i * cellSize + yOffset, boardSize * cellSize + xOffset, i * cellSize + yOffset);
-       }
+            // Добавляем цифры слева
+            if (i > 0 && i <= boardSize) {
+                String number = Integer.toString(i);
+                int numberX = xOffset / 2 - g2d.getFontMetrics().stringWidth(number) / 2;
+                int numberY = i * cellSize + yOffset - cellSize / 2 + g2d.getFontMetrics().getAscent() / 2;
+                g2d.drawString(number, numberX, numberY);
+            }
+        }
 
-       // Рисуем разделительную полосу
-       g2d.drawLine(boardSize * cellSize + xOffset * 2, 0, boardSize * cellSize + xOffset * 2, getHeight());
+        for (int i = 0; i <= boardSize; i++) {
+            g2d.drawLine(xOffset, i * cellSize + yOffset, boardSize * cellSize + xOffset, i * cellSize + yOffset);
 
-       // Рисуем сетку для поля противника
-       int enemyXOffset = getWidth() / 2 + boardSize * cellSize / 4; // Смещение вправо от разделительной полосы
-       int enemyYOffset = yOffset; // Также вертикальное смещение для правой половины окна
+            // Добавляем буквы сверху
+            if (i > 0 && i <= boardSize) {
+                char letter = (char) ('A' + i - 1);
+                String letterStr = Character.toString(letter);
+                int letterX = i * cellSize + xOffset - cellSize / 2 - g2d.getFontMetrics().stringWidth(letterStr) / 2;
+                int letterY = yOffset / 2 + g2d.getFontMetrics().getAscent() / 2;
+                g2d.drawString(letterStr, letterX, letterY);
+            }
+        }
 
-       // Рисуем сетку для правой половины поля противника
-       for (int i = 0; i <= boardSize; i++) {
-           g2d.drawLine(i * cellSize + enemyXOffset, enemyYOffset, i * cellSize + enemyXOffset, boardSize * cellSize + enemyYOffset);
-       }
+        // Рисуем разделительную полосу
+        g2d.drawLine(boardSize * cellSize + xOffset * 2, 0, boardSize * cellSize + xOffset * 2, getHeight());
 
-       for (int i = 0; i <= boardSize; i++) {
-           g2d.drawLine(enemyXOffset, i * cellSize + enemyYOffset, boardSize * cellSize + enemyXOffset, i * cellSize + enemyYOffset);
-       }
-   }
+        // Рисуем сетку для поля противника
+        int enemyXOffset = getWidth() / 2 + boardSize * cellSize / 4; // Смещение вправо от разделительной полосы
+        int enemyYOffset = yOffset; // Также вертикальное смещение для правой половины окна
+
+        // Рисуем сетку для правой половины поля противника
+        for (int i = 0; i <= boardSize; i++) {
+            g2d.drawLine(i * cellSize + enemyXOffset, enemyYOffset, i * cellSize + enemyXOffset, boardSize * cellSize + enemyYOffset);
+
+            // Добавляем цифры справа
+            if (i > 0 && i <= boardSize) {
+                String number = Integer.toString(i);
+                int numberX = boardSize * cellSize + xOffset * 2 + xOffset / 2 - g2d.getFontMetrics().stringWidth(number) / 2;
+                int numberY = i * cellSize + enemyYOffset - cellSize / 2 + g2d.getFontMetrics().getAscent() / 2;
+                g2d.drawString(number, numberX, numberY);
+            }
+        }
+
+        for (int i = 0; i <= boardSize; i++) {
+            g2d.drawLine(boardSize * cellSize + xOffset * 2, i * cellSize + enemyYOffset, getWidth(), i * cellSize + enemyYOffset);
+
+            // Добавляем буквы сверху
+            if (i > 0 && i <= boardSize) {
+                char letter = (char) ('A' + i - 1);
+                String letterStr = Character.toString(letter);
+                int letterX = boardSize * cellSize + xOffset * 2 + i * cellSize + xOffset - cellSize / 2 - g2d.getFontMetrics().stringWidth(letterStr) / 2;
+                int letterY = yOffset / 2 + g2d.getFontMetrics().getAscent() / 2;
+                g2d.drawString(letterStr, letterX, letterY);
+            }
+        }
+    }
+
 
 
     private void drawShips(Graphics2D g2d) {
