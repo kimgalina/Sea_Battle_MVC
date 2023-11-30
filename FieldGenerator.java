60dd43dd747dcafdbabe7bd76bb119f3d4cb3d1a
kimgalina <genrietta.kim@gmail.com>
2023-11-30 20:43:32 +0600

    deleted unnecessary printing

--- FieldGenerator.java
+++ FieldGenerator.java
@@ -14,7 +14,6 @@ public class FieldGenerator {
         for(int i = 4,k = 1; i > 0; i--,k++) {
             for(int j = k; j > 0; j--) {
                 buildShip(i, matrix);
-                System.out.println("build ship with " + i + " decks");
             }
         }
 
@@ -34,20 +33,16 @@ public class FieldGenerator {
         int direction;
         do {
             do{
-                System.out.println("generating new coordinates >>>");
                 x = generateCoordinate();
                 y = generateCoordinate();
-                System.out.println("x = " + x + " y = " + y);
             } while(matrix[x][y] != 0);// while cell is not empty
             direction = generateDirection();
-            System.out.println("direction = " + direction);
         } while (!isDirectionValid(decksCount, matrix, direction, x, y));
         setShip(x, y, direction, decksCount, matrix);
 
     }
 
     private void setShip(int x , int y, int direction, int decksCount, int[][] matrix) {
-        System.out.println("SET SHIP");
         switch(direction) {
             case 0:// up
                for(int i = 0; i < decksCount; i++, x--) {
@@ -87,62 +82,48 @@ public class FieldGenerator {
     private int isValidAngle(int x, int y, int[][] matrix) { // return 0 if it is valid angle , 1 if it is not angle or -1 not valid angle
         if(x == 0 && y == 0) {
             if (matrix[x][y+1] != 0 || matrix[x + 1][y + 1] != 0 || matrix[x + 1][y] != 0 ) {
-                // if there is other ship
-                System.out.println("NOT valid angle");
                 return -1;
             }
-            System.out.println("Valid angle");
             return 0;
         } else if (x == 0 && y == 9) {
             if (matrix[x][y-1] != 0 || matrix[x + 1][y - 1] != 0 || matrix[x + 1][y] != 0 ) {
-                // if there is other ship
                 return -1;
             }
-            System.out.println("Valid angle");
             return 0;
         } else if (y == 0 && x == 9) {
             if (matrix[x][y+1] != 0 || matrix[x - 1][y + 1] != 0 || matrix[x - 1][y] != 0) {
-                // if there is other ship
                 return -1;
             }
-            System.out.println("Valid angle");
             return 0;
         } else if (x == 9 && y == 9) {
             if (matrix[x][y-1] != 0 || matrix[x - 1][y - 1] != 0 || matrix[x - 1][y] != 0) {
-                // if there is other ship
                 return -1;
             }
-            System.out.println("Valid angle");
             return 0;
         }
-        System.out.println("not angle");
         return 1;
     }
     private boolean isValidCell(int x, int y, int[][] matrix) {
         if(x == 0) {
             if (matrix[x][y-1] != 0 || matrix[x][y+1] != 0 || matrix[x + 1][y - 1] != 0 || matrix[x + 1][y + 1] != 0 || matrix[x + 1][y] != 0) {
-                // if there is other ship
                 return false;
             }
         } else if (y == 0) {
             if (matrix[x][y+1] != 0 || matrix[x + 1][y + 1] != 0 || matrix[x - 1][y + 1] != 0 || matrix[x + 1][y] != 0 || matrix[x - 1][y] != 0) {
-                // if there is other ship
                 return false;
             }
         } else if (x == 9) {
             if (matrix[x][y-1] != 0 || matrix[x][y+1] != 0 || matrix[x - 1][y - 1] != 0 || matrix[x - 1][y + 1] != 0 || matrix[x - 1][y] != 0) {
-                // if there is other ship
                 return false;
             }
         } else if (y == 9) {
             if (matrix[x][y-1] != 0 || matrix[x + 1][y - 1] != 0 || matrix[x - 1][y - 1] != 0 || matrix[x + 1][y] != 0 || matrix[x - 1][y] != 0) {
-                // if there is other ship
                 return false;
             }
         } else {
             if (matrix[x][y-1] != 0 || matrix[x][y+1] != 0 || matrix[x + 1][y - 1] != 0 || matrix[x + 1][y + 1] != 0 ||
                     matrix[x - 1][y - 1] != 0 || matrix[x - 1][y + 1] != 0 || matrix[x + 1][y] != 0 || matrix[x - 1][y] != 0) {
-                // if there is other ship
+
                 return false;
             }
         }
@@ -163,9 +144,7 @@ public class FieldGenerator {
     }
 
     private boolean checkUp(int decksCount, int x, int y, int[][] matrix) {
-        System.out.println("Check UP");
         for(int i = 0; i < decksCount; i++, x--) {
-            System.out.println("x = " + x + "  y = " + y);
             if(x < 0 || !checkCell(x, y, matrix)) {
                 return false;
             }
@@ -174,9 +153,7 @@ public class FieldGenerator {
     }
 
     private boolean checkRight(int decksCount, int x, int y, int[][] matrix) {
-        System.out.println("Check Right");
         for(int i = 0; i < decksCount; i++, y++) {
-            System.out.println("x = " + x + "  y = " + y);
             if(y > 9 || !checkCell(x, y, matrix)) {
                 return false;
             }
@@ -185,9 +162,7 @@ public class FieldGenerator {
     }
 
     private boolean checkDown(int decksCount, int x, int y, int[][] matrix) {
-        System.out.println("Check DOWN");
         for(int i = 0; i < decksCount; i++, x++) {
-            System.out.println("x = " + x + "  y = " + y);
             if(x > 9 || !checkCell(x, y, matrix)) {
                 return false;
             }
@@ -196,9 +171,7 @@ public class FieldGenerator {
     }
 
     private boolean checkLeft(int decksCount, int x, int y, int[][] matrix) {
-        System.out.println("Check LEFT");
         for(int i = 0; i < decksCount; i++, y--) {
-            System.out.println("x = " + x + "  y = " + y);
             if(y < 0 || !checkCell(x, y, matrix)) {
                 return false;
             }
--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
         int[][] field =  generator.generateField();
         for(int i = 0; i < field.length; i++) {
            for(int j = 0; j < field[i].length; j++) {
-               System.out.print(field[i][j] + "  ");
+               System.out.print(field[i][j] + " ");
            }
            System.out.println();
        }
