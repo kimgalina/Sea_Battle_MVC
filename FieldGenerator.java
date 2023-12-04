caf3dcf55d307325fd3ab32709d056f1c0c2d3a4
kimgalina <genrietta.kim@gmail.com>
2023-12-04 22:39:26 +0600

    rewrite code for matrix of cells and added logic of assignment matching image for each cell

--- FieldGenerator.java
+++ FieldGenerator.java
@@ -1,5 +1,8 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
-
+import java.awt.Image;
+import javax.imageio.ImageIO;
 
 public class FieldGenerator {
 
@@ -10,17 +13,18 @@ public class FieldGenerator {
     }
 
     public Cell[][] getGeneratedField() {
-        int[][] field = generateField();
-        Cell[][] generatedField = new Cell[field.length][10];
-        for(int i = 0; i < field.length; i++) {
-            for(int j = 0; j < field[i].length; j++) {
-                generatedField[i][j] = new Cell(i * 50 + 50, j * 50 + 100, 50, 50, field[i][j]);
+        Cell[][] generatedField = generateField();
+        for(int i = 0; i < generatedField.length; i++) {
+            for(int j = 0; j < generatedField[i].length; j++) {
+                System.out.print(generatedField[i][j].getValue() + " ");
             }
+            System.out.println();
         }
         return generatedField;
     }
-    private int[][] generateField() {
-        int[][] matrix = new int[10][10];
+    private Cell[][] generateField() {
+        Cell[][] matrix = new Cell[10][10];
+        initializeField(matrix);
         for (int i = 4, k = 1; i > 0; i--, k++) {
             for (int j = k; j > 0; j--) {
                 buildShip(i, matrix);
@@ -28,6 +32,14 @@ public class FieldGenerator {
         }
 
         return matrix;
+    }
+
+    private void initializeField(Cell[][] matrix) {
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = new Cell(i * 50 + 50, j * 50 + 100, 50, 50, 0);
+            }
+        }
     }
 
     private int generateCoordinate() {
@@ -38,7 +50,7 @@ public class FieldGenerator {
         return random.nextInt(4);
     }
 
-    private void buildShip(int decksCount, int[][] matrix) {
+    private void buildShip(int decksCount, Cell[][] matrix) {
         int x;
         int y;
         int direction;
@@ -46,39 +58,64 @@ public class FieldGenerator {
             do {
                 x = generateCoordinate();
                 y = generateCoordinate();
-            } while (matrix[x][y] != 0);// while cell is not empty
+            } while (matrix[x][y].getValue() != 0);// while cell is not empty
             direction = generateDirection();
         } while (!isDirectionValid(decksCount, matrix, direction, x, y));
-        setShip(x, y, direction, decksCount, matrix);
+        try {
+            setShip(x, y, direction, decksCount, matrix);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    private void setShip(int x, int y, int direction, int decksCount, int[][] matrix) {
+    private void setShip(int x, int y, int direction, int decksCount, Cell[][] matrix) throws IOException {
         switch (direction) {
             case 0:// up
-                for (int i = 0; i < decksCount; i++, x--) {
-                    matrix[x][y] = 1;
+                for (int i = decksCount; i > 0; i--, x--) {
+                    matrix[x][y].setValue(1);
+
+                    // каждой клетке в зависимости от ее положения ставим картинку части корабля
+                    String imagePath = decksCount + "ship-" + i + ".png";
+                    File file = new File(imagePath);
+                    matrix[x][y].setImage(ImageIO.read(file));
                 }
+
                 return;
             case 1:// right
-                for (int i = 0; i < decksCount; i++, y++) {
-                    matrix[x][y] = 1;
+                for (int i = 1; i <= decksCount; i++, y++) {
+                    matrix[x][y].setValue(1);
+
+                    // каждой клетке в зависимости от ее положения ставим картинку части корабля
+                    String imagePath = decksCount + "ship-" + i + ".png";
+                    File file = new File(imagePath);
+                    matrix[x][y].setImage(ImageIO.read(file));
                 }
                 return;
             case 2:// down
-                for (int i = 0; i < decksCount; i++, x++) {
-                    matrix[x][y] = 1;
+                for (int i = 1; i <= decksCount; i++, x++) {
+                    matrix[x][y].setValue(1);
+
+                    // каждой клетке в зависимости от ее положения ставим картинку части корабля
+                    String imagePath = decksCount + "ship-" + i + ".png";
+                    File file = new File(imagePath);
+                    matrix[x][y].setImage(ImageIO.read(file));
                 }
                 return;
             case 3:// left
-                for (int i = 0; i < decksCount; i++, y--) {
-                    matrix[x][y] = 1;
+                for (int i = 1; i <= decksCount; i++, y--) {
+                    matrix[x][y].setValue(1);
+
+                    // каждой клетке в зависимости от ее положения ставим картинку части корабля
+                    String imagePath = decksCount + "ship-" + i + ".png";
+                    File file = new File(imagePath);
+                    matrix[x][y].setImage(ImageIO.read(file));
                 }
                 return;
         }
     }
 
-    private boolean isDirectionValid(int decksCount, int[][] matrix, int direction, int x, int y) {
+    private boolean isDirectionValid(int decksCount, Cell[][] matrix, int direction, int x, int y) {
         switch (direction) {
             case 0:// up
                 return checkUp(decksCount, x, y, matrix);
@@ -92,24 +129,24 @@ public class FieldGenerator {
         return false;
     }
 
-    private int isValidAngle(int x, int y, int[][] matrix) { // return 0 if it is valid angle , 1 if it is not angle or -1 not valid angle
+    private int isValidAngle(int x, int y, Cell[][] matrix) { // return 0 if it is valid angle , 1 if it is not angle or -1 not valid angle
         if (x == 0 && y == 0) {
-            if (matrix[x][y + 1] != 0 || matrix[x + 1][y + 1] != 0 || matrix[x + 1][y] != 0) {
+            if (matrix[x][y + 1].getValue() != 0 || matrix[x + 1][y + 1].getValue() != 0 || matrix[x + 1][y].getValue() != 0) {
                 return -1;
             }
             return 0;
         } else if (x == 0 && y == 9) {
-            if (matrix[x][y - 1] != 0 || matrix[x + 1][y - 1] != 0 || matrix[x + 1][y] != 0) {
+            if (matrix[x][y - 1].getValue() != 0 || matrix[x + 1][y - 1].getValue() != 0 || matrix[x + 1][y].getValue() != 0) {
                 return -1;
             }
             return 0;
         } else if (y == 0 && x == 9) {
-            if (matrix[x][y + 1] != 0 || matrix[x - 1][y + 1] != 0 || matrix[x - 1][y] != 0) {
+            if (matrix[x][y + 1].getValue() != 0 || matrix[x - 1][y + 1].getValue() != 0 || matrix[x - 1][y].getValue() != 0) {
                 return -1;
             }
             return 0;
         } else if (x == 9 && y == 9) {
-            if (matrix[x][y - 1] != 0 || matrix[x - 1][y - 1] != 0 || matrix[x - 1][y] != 0) {
+            if (matrix[x][y - 1].getValue() != 0 || matrix[x - 1][y - 1].getValue() != 0 || matrix[x - 1][y].getValue() != 0) {
                 return -1;
             }
             return 0;
@@ -117,26 +154,26 @@ public class FieldGenerator {
         return 1;
     }
 
-    private boolean isValidCell(int x, int y, int[][] matrix) {
+    private boolean isValidCell(int x, int y, Cell[][] matrix) {
         if (x == 0) {
-            if (matrix[x][y - 1] != 0 || matrix[x][y + 1] != 0 || matrix[x + 1][y - 1] != 0 || matrix[x + 1][y + 1] != 0 || matrix[x + 1][y] != 0) {
+            if (matrix[x][y - 1].getValue() != 0 || matrix[x][y + 1].getValue() != 0 || matrix[x + 1][y - 1].getValue() != 0 || matrix[x + 1][y + 1].getValue() != 0 || matrix[x + 1][y].getValue() != 0) {
                 return false;
             }
         } else if (y == 0) {
-            if (matrix[x][y + 1] != 0 || matrix[x + 1][y + 1] != 0 || matrix[x - 1][y + 1] != 0 || matrix[x + 1][y] != 0 || matrix[x - 1][y] != 0) {
+            if (matrix[x][y + 1].getValue() != 0 || matrix[x + 1][y + 1].getValue() != 0 || matrix[x - 1][y + 1].getValue() != 0 || matrix[x + 1][y].getValue() != 0 || matrix[x - 1][y].getValue() != 0) {
                 return false;
             }
         } else if (x == 9) {
-            if (matrix[x][y - 1] != 0 || matrix[x][y + 1] != 0 || matrix[x - 1][y - 1] != 0 || matrix[x - 1][y + 1] != 0 || matrix[x - 1][y] != 0) {
+            if (matrix[x][y - 1].getValue() != 0 || matrix[x][y + 1].getValue() != 0 || matrix[x - 1][y - 1].getValue() != 0 || matrix[x - 1][y + 1].getValue() != 0 || matrix[x - 1][y].getValue() != 0) {
                 return false;
             }
         } else if (y == 9) {
-            if (matrix[x][y - 1] != 0 || matrix[x + 1][y - 1] != 0 || matrix[x - 1][y - 1] != 0 || matrix[x + 1][y] != 0 || matrix[x - 1][y] != 0) {
+            if (matrix[x][y - 1].getValue() != 0 || matrix[x + 1][y - 1].getValue() != 0 || matrix[x - 1][y - 1].getValue() != 0 || matrix[x + 1][y].getValue() != 0 || matrix[x - 1][y].getValue() != 0) {
                 return false;
             }
         } else {
-            if (matrix[x][y - 1] != 0 || matrix[x][y + 1] != 0 || matrix[x + 1][y - 1] != 0 || matrix[x + 1][y + 1] != 0 ||
-                    matrix[x - 1][y - 1] != 0 || matrix[x - 1][y + 1] != 0 || matrix[x + 1][y] != 0 || matrix[x - 1][y] != 0) {
+            if (matrix[x][y - 1].getValue() != 0 || matrix[x][y + 1].getValue() != 0 || matrix[x + 1][y - 1].getValue() != 0 || matrix[x + 1][y + 1].getValue() != 0 ||
+                    matrix[x - 1][y - 1].getValue() != 0 || matrix[x - 1][y + 1].getValue() != 0 || matrix[x + 1][y].getValue() != 0 || matrix[x - 1][y].getValue() != 0) {
 
                 return false;
             }
@@ -144,7 +181,7 @@ public class FieldGenerator {
         return true;
     }
 
-    private boolean checkCell(int x, int y, int[][] matrix) {
+    private boolean checkCell(int x, int y, Cell[][] matrix) {
         int result = isValidAngle(x, y, matrix);
         switch (result) {
             case 0:
@@ -157,7 +194,7 @@ public class FieldGenerator {
         return false;
     }
 
-    private boolean checkUp(int decksCount, int x, int y, int[][] matrix) {
+    private boolean checkUp(int decksCount, int x, int y, Cell[][] matrix) {
         for (int i = 0; i < decksCount; i++, x--) {
             if (x < 0 || !checkCell(x, y, matrix)) {
                 return false;
@@ -166,7 +203,7 @@ public class FieldGenerator {
         return true;
     }
 
-    private boolean checkRight(int decksCount, int x, int y, int[][] matrix) {
+    private boolean checkRight(int decksCount, int x, int y, Cell[][] matrix) {
         for (int i = 0; i < decksCount; i++, y++) {
             if (y > 9 || !checkCell(x, y, matrix)) {
                 return false;
@@ -175,7 +212,7 @@ public class FieldGenerator {
         return true;
     }
 
-    private boolean checkDown(int decksCount, int x, int y, int[][] matrix) {
+    private boolean checkDown(int decksCount, int x, int y, Cell[][] matrix) {
         for (int i = 0; i < decksCount; i++, x++) {
             if (x > 9 || !checkCell(x, y, matrix)) {
                 return false;
@@ -184,7 +221,7 @@ public class FieldGenerator {
         return true;
     }
 
-    private boolean checkLeft(int decksCount, int x, int y, int[][] matrix) {
+    private boolean checkLeft(int decksCount, int x, int y, Cell[][] matrix) {
         for (int i = 0; i < decksCount; i++, y--) {
             if (y < 0 || !checkCell(x, y, matrix)) {
                 return false;
--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String args[]) {
+        FieldGenerator generator = new FieldGenerator();
+        generator.getGeneratedField();
         Viewer viewer = new Viewer();
-    }
+    }git add
 }
