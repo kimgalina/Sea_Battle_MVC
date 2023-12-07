952d6af6b8ba97eb251a2412701a641875045ac0
Diyorka <diyorp2@mail.ru>
2023-12-07 13:44:39 +0600

    Added splash screen

---
+++ SplashScreen.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SplashScreen {
+
+    JFrame frame;
+    JProgressBar progressBar;
+
+    public SplashScreen() {
+        frame = new JFrame();
+        frame.getContentPane().setLayout(null);
+        frame.setUndecorated(true);
+        frame.setSize(1200, 720);
+        frame.setLocation(300, 100);
+        frame.setLocationRelativeTo(null);
+        JLabel backgroundImage = new JLabel(new ImageIcon("images/SplashBackground.png"));
+        backgroundImage.setSize(1200, 720);
+
+        frame.add(backgroundImage);
+        frame.setVisible(true);
+
+        addProgressBar();
+        runningProgressBar();
+    }
+
+    private void addProgressBar() {
+        progressBar = new JProgressBar();
+        progressBar.setBounds(100, 620, 1000, 50);
+        progressBar.setBorderPainted(true);
+        progressBar.setStringPainted(true);
+        progressBar.setBackground(Color.WHITE);
+        progressBar.setForeground(Color.BLACK);
+        progressBar.setValue(0);
+        frame.add(progressBar);
+    }
+
+    public void runningProgressBar() {
+        int i = 0;
+
+        while (i <= 100) {
+            try {
+                Thread.sleep(50);
+                progressBar.setValue(i);
+                i += 2.5;
+                if (i >= 100) {
+                    frame.dispose();
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
