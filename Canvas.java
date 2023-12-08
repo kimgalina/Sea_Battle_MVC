62bd3acff968c56d50e943304c6e4364e603a780
NursTaal <nursultantaalaibekov01@gmail.com>
2023-12-09 01:10:43 +0600

    update canvas

--- Canvas.java
+++ Canvas.java
@@ -1,20 +1,48 @@
 import javax.swing.JPanel;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.geom.RoundRectangle2D;
+import java.awt.AlphaComposite;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
 @SuppressWarnings("serial")
 public class Canvas extends JPanel {
 
-    private Model model;
+    private final Model model;
+    private final Image backgroundImage;
+    private final Image seaBattleImage;
+    private final Font font;
+    private final Font numberFont;
+    private final RoundRectangle2D exitButton;
+    private final RoundRectangle2D restartButton;
+    private final RoundRectangle2D startButton;
 
     public Canvas(Model model, Controller controller) {
+        backgroundImage = new ImageIcon("images/background.jpg").getImage();
+        seaBattleImage = new ImageIcon("images/sea-battle-cell.png").getImage();
+
+        font = new Font("Franklin Gothic Heavy", Font.PLAIN, 25);
+        numberFont = new Font("Franklin Gothic Heavy", Font.PLAIN, 30);
+        int arcWidth = 30;
+        int arcHeight = 30;
+
+        makeImageTransparent(backgroundImage, 1f, 0 , 0);
+        makeImageTransparent(seaBattleImage, 1f, 50, 50);
+
+        JLabel backgroundLabel = new JLabel(new ImageIcon(backgroundImage));
+        backgroundLabel.setSize(1200,720);
+        add(backgroundLabel);
+
+        exitButton = new RoundRectangle2D.Double(100, 620, 100, 50, arcWidth, arcHeight);
+        restartButton = new RoundRectangle2D.Double(250, 620, 100, 50, arcWidth, arcHeight);
+        startButton = new RoundRectangle2D.Double(400, 620, 100, 50, arcWidth, arcHeight);
 
         this.model = model;
         addMouseListener(controller);
@@ -25,32 +53,36 @@ public class Canvas extends JPanel {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
 
-        drawGrid(g2d);
-        drawBoards(g2d);
+//        float alpha = 0.5f;
+//        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+
+//        drawGrid(g2d);
+//        drawBoards(g2d);
+    }
+
+    private Image makeImageTransparent(Image image, float alpha, int x, int y) {
+        BufferedImage bufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+        g2d.drawImage(image, x, y, this);
+        g2d.dispose();
+        return bufferedImage;
     }
 
 
     private void drawGrid(Graphics2D g2d) {
         drawBackground(g2d);
-        drawSeaBattleImages(g2d);
         drawBoardNames(g2d);
         drawButtons(g2d);
     }
 
     private void drawBackground(Graphics2D g2d) {
-        Image backgroundImage = new ImageIcon("images/background.jpg").getImage();
-        g2d.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
-    }
-
-    private void drawSeaBattleImages(Graphics2D g2d) {
-        Image seaBattleImage = new ImageIcon("images/sea-battle-cell.png").getImage();
+//        g2d.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
         g2d.drawImage(seaBattleImage, 0, 50, 600, 600, this);
         g2d.drawImage(seaBattleImage, 600, 50, 600, 600, this);
     }
 
     private void drawBoardNames(Graphics2D g2d) {
-        Font numberFont = new Font("Franklin Gothic Heavy", Font.PLAIN, 30);
-
         drawBoardName(g2d, numberFont, Color.BLACK, "User's Board", 240, 30);
         drawBoardName(g2d, numberFont, new Color(153, 0, 0), "Computer's Board", 780, 30);
     }
@@ -62,14 +94,6 @@ public class Canvas extends JPanel {
     }
 
     private void drawButtons(Graphics2D g2d) {
-        Font font = new Font("Franklin Gothic Heavy", Font.PLAIN, 25);
-        int arcWidth = 30;
-        int arcHeight = 30;
-
-        RoundRectangle2D exitButton = new RoundRectangle2D.Double(100, 620, 100, 50, arcWidth, arcHeight);
-        RoundRectangle2D restartButton = new RoundRectangle2D.Double(250, 620, 100, 50, arcWidth, arcHeight);
-        RoundRectangle2D startButton = new RoundRectangle2D.Double(400, 620, 100, 50, arcWidth, arcHeight);
-
         drawButton(g2d, Color.RED, "Exit", 120, 655, font, exitButton);
         drawButton(g2d, new Color(189, 189, 0), "Restart", 260, 655, font, restartButton);
         drawButton(g2d, new Color(0, 102, 0), "Start", 420, 655, font, startButton);
